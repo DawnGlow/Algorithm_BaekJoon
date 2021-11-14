dff07b0850106743d0dfc58b180529038dd3964d
DawnGlow <69619752+DawnGlow@users.noreply.github.com>
2021-11-14 20:15:02 +0900

    211114

---
+++ 11654/11654/11654.cpp
@@ -0,0 +1,10 @@
+#include<cstdio>
+main(){char a;scanf("%c",&a);printf("%d",a);}
+/*
+다른 방법
+#import<ios>
+main(){printf("%d",getchar());}
+
+--> %d 뒤에 꼭 변수가 오라는 법은 없다.
+getchar()나 gets함수도 알아두자.
+*/
