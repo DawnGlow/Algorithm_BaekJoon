cce9a59eda1d066256056cb0436c4804e8a839bd
DawnGlow <69619752+DawnGlow@users.noreply.github.com>
2021-12-16 22:25:31 +0900

    211216
    incompletion code

---
+++ 1011/1011/1011.cpp
@@ -0,0 +1,14 @@
+//incompletion code
+#include <iostream>
+using namespace std;
+main () {
+    int T;
+    cin >> T;
+    for (int i = 0; i++ < T;) {
+        int x, y, k, m(1);
+        cin >> x >> y;
+        while (x + k != y) {
+            
+        }
+    }
+}
