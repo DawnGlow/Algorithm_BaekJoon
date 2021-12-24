1a82bcabc2b9c6a726e3bf296b2352e0008e2422
DawnGlow <69619752+DawnGlow@users.noreply.github.com>
2021-12-24 22:22:24 +0900

    211224
    /*
    처음엔 result라는 변수를 매개변수로 추가하여 함수를 만들었는데, 메모리를 크게 차지하게 되어 문제가 발생했다.
    그래서 변수를 1개로 줄이고, 추가적인 변수를 만들지 않았다.
    근데 이게 문제가 아니라, 문제 조건이 N은 0 이상이였다...
    */

--- 10872/10872/10872.cpp
+++ 10872/10872/10872.cpp
@@ -1,7 +1,7 @@
 //completion code
 #include <iostream>
 int factorial(int x) {
-    if (x == 1) {
+    if (x <= 1) {
         return 1;
     }
     return x * factorial(x - 1);
@@ -15,4 +15,5 @@ main () {
 /*
 처음엔 result라는 변수를 매개변수로 추가하여 함수를 만들었는데, 메모리를 크게 차지하게 되어 문제가 발생했다.
 그래서 변수를 1개로 줄이고, 추가적인 변수를 만들지 않았다.
+근데 이게 문제가 아니라, 문제 조건이 N은 0 이상이였다...
 */
