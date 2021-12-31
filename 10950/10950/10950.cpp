65d14c83225291d90f0f1074a4f422f5903cea27
DawnGlow <69619752+DawnGlow@users.noreply.github.com>
2021-12-31 22:46:58 +0900

    211231

---
+++ 10950/10950/10950.cpp
@@ -0,0 +1,10 @@
+#import <iostream>
+using namespace std;
+int main(void) {
+    int a, b, T;
+    cin >> T;
+    for (int i = 0; i++ < T;) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
